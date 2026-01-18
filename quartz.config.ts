db5b9a09f0afc53148a89bf7c88524d661cd93aa
Lseoksee <da864268@naver.com>
2026-01-18 18:39:05 +0900

    무시목록추가

--- quartz.config.ts
+++ quartz.config.ts
@@ -17,7 +17,7 @@ const config: QuartzConfig = {
     },
     locale: "ko-KR",
     baseUrl: "https://lseoksee.github.io",
-    ignorePatterns: ["private", "templates", ".obsidian"],
+    ignorePatterns: ["private", "templates", ".obsidian", "*-pv.md"],
     defaultDateType: "created",
     theme: {
       fontOrigin: "googleFonts",
