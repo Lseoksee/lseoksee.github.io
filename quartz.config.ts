c194016c5ad629ef13227f5177307a629d942828
Lseoksee <da864268@naver.com>
2026-01-18 19:18:13 +0900

    패턴 수정

--- quartz.config.ts
+++ quartz.config.ts
@@ -17,7 +17,7 @@ const config: QuartzConfig = {
     },
     locale: "ko-KR",
     baseUrl: "https://lseoksee.github.io",
-    ignorePatterns: ["private", "templates", ".obsidian", "**/*-pv/", "*-pv.md"],
+    ignorePatterns: ["private", "templates", ".obsidian", "**/*-pv/**", "*-pv.md"],
     defaultDateType: "created",
     theme: {
       fontOrigin: "googleFonts",
